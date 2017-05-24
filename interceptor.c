376eafa5557f46470c7f2312be4eba4ef29fde04
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-05-24 19:24:12 -0400

    fix

--- interceptor.c
+++ interceptor.c
@@ -336,13 +336,13 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 	if(syscall < 0 || syscall > NR_syscalls || syscall == MY_CUSTOM_SYSCALL || pid < 0 || pid_task(find_vpid(pid), PIDTYPE_PID) != NULL){
 		return -EINVAL;
 	} else if(cmd == REQUEST_SYSCALL_INTERCEPT){
-	 	if (current_uid != 0){
+	 	if (current_uid() != 0){
 			return -EPERM;
 		} else if (table[syscall].intercepted != 0) {
 			return -EBUSY;
 		}
 	} else if(cmd == REQUEST_SYSCALL_RELEASE){
-		if (current_uid != 0){
+		if (current_uid() != 0){
 			return -EPERM;
 		} else if(table[syscall].intercepted == 0) {
 			return -EINVAL;
@@ -352,7 +352,7 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 			return -EINVAL;
 		} else if (check_pid_monitored) {
 			return -EBUSY;
-		} else if (pid == 0 && current_uid != 0){
+		} else if (pid == 0 && current_uid() != 0){
 			return -EPERM;
 		} else if (!check_pid_from_list(pid, current->pid)){
 			return -EPERM;
@@ -360,7 +360,7 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 	} else if(cmd == REQUEST_STOP_MONITORING){
 		if (!check_pid_monitored) {
 			return -EINVAL;
-		} else if (pid == 0 && current_uid != 0){
+		} else if (pid == 0 && current_uid() != 0){
 			return -EPERM'
 		} else if (!check_pid_from_list(pid, current->pid)){
 			return -EPERM;
