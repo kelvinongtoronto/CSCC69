1e873ee92d3c769bb7631d4333e6597f921fbd67
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-05-26 17:52:48 -0400

    init/exit

--- interceptor.c
+++ interceptor.c
@@ -429,13 +429,14 @@ long (*orig_custom_syscall)(void);
  * - Ensure synchronization as needed.
  */
 static int init_function(void) {
-
-
-
-
-
-
-
+	orig_custom_syscall = sys_call_table[MY_CUSTOM_SYSCALL];
+	orig_exit_group = sys_call_table[__NR_exit_group];
+	spin_lock(&calltable_lock);
+	set_addr_rw((unsigned long) sys_call_table);
+	sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
+	sys_call_table[__NR_exit_group] = my_exit_group;
+	set_addr_ro((unsigned long) sys_call_table);
+	spin_unlock(&calltable_lock);
 	return 0;
 }
 
@@ -451,12 +452,12 @@ static int init_function(void) {
  */
 static void exit_function(void)
 {        
-
-
-
-
-
-
+	spin_lock(&calltable_lock);
+	set_addr_rw((unsigned long) sys_call_table);
+	sys_call_table[MY_CUSTOM_SYSCALL] = orig_custom_syscall;
+	sys_call_table[__NR_exit_group] = orig_exit_group;
+	set_addr_ro((unsigned long) sys_call_table);
+	spin_unlock(&calltable_lock);
 }
 
 module_init(init_function);
