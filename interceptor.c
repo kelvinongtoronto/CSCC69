c34a3a6b09546f0d48283fd5a467d54ddb7b4823
chrisPbit <chrisPbit@gmail.com>
2017-05-26 15:26:54 -0400

    Update interceptor.c

--- interceptor.c
+++ interceptor.c
@@ -350,8 +350,8 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 			//lock 
 			table[syscall].intercepted = 1;
 			//table[syscall].f = sys_call_table[syscall]; //pt_reg
-			interceptor(table[syscall].*f);
-			
+			//sys_call_table[syscall] = interceptor(table[syscall].*f);
+			table[syscall] = interceptor();
 			//interceptor(table[syscall].*f);
 			set_addr_ro((unsigned long) sys_call_table);
 		}
