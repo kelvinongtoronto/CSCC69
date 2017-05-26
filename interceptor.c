a4d8551e91c54ba7be7e05db1cf3e902a9175f8b
chrisPbit <chrisPbit@gmail.com>
2017-05-26 19:54:59 -0400

    Update interceptor.c

--- interceptor.c
+++ interceptor.c
@@ -430,6 +430,7 @@ long (*orig_custom_syscall)(void);
  */
 static int init_function(void) {
 	printk(KERN_INFO "Test init");
+	/***
 	orig_custom_syscall = sys_call_table[MY_CUSTOM_SYSCALL];
 	orig_exit_group = sys_call_table[__NR_exit_group];
 	spin_lock(&calltable_lock);
@@ -438,6 +439,7 @@ static int init_function(void) {
 	sys_call_table[__NR_exit_group] = my_exit_group;
 	set_addr_ro((unsigned long) sys_call_table);
 	spin_unlock(&calltable_lock);
+	***/
 	return 0;
 }
 
@@ -454,12 +456,14 @@ static int init_function(void) {
 static void exit_function(void)
 {        
 	printk(KERN_INFO "Test exit");
+	/**
 	spin_lock(&calltable_lock);
 	set_addr_rw((unsigned long) sys_call_table);
 	sys_call_table[MY_CUSTOM_SYSCALL] = orig_custom_syscall;
 	sys_call_table[__NR_exit_group] = orig_exit_group;
 	set_addr_ro((unsigned long) sys_call_table);
 	spin_unlock(&calltable_lock);
+	***/
 }
 
 module_init(init_function);
