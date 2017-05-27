f56599bff16e06f98c94c299afe30b1a38de0b10
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-05-27 16:50:28 -0400

    tester

--- test_full.c
+++ test_full.c
@@ -175,7 +175,12 @@ void test_syscall(int syscall) {
 	do_start(syscall, -2, -EINVAL);
 	do_start(syscall, 0, 0);
 	do_stop(syscall, 0, 0);
-
+	//do_start(syscall, 1, 0);
+	//do_as_guest("./test_full stop %d 1 %d", syscall, -EPERM);
+	//do_stop(syscall, 1, 0);
+	//do_as_guest("./test_full start %d -1 %d", syscall, 0);
+	//do_stop(syscall, last_child, -EINVAL);
+	do_release(syscall, 0);
 }
 
 
