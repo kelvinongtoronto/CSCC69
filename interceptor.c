c33edaf5745309c1e6d3c56626da9ff19a6b3c52
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-05-27 17:13:35 -0400

    try2

--- interceptor.c
+++ interceptor.c
@@ -411,9 +411,12 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 	} else if(cmd == REQUEST_STOP_MONITORING){
 		if (!check_pid_monitored(syscall,pid)) {
 			return -EINVAL;
+		} else if (pid == 0 && current_uid() != 0){
+			return -EPERM;
 		}  else if (pid == 0) {
 			spin_lock(&pidlist_lock);
 			destroy_list(syscall);
+			table[syscall].monitored = 0;
 			spin_unlock(&pidlist_lock);
 			return 0;
 		} else if (pid_task(find_vpid(pid), PIDTYPE_PID) == NULL) {
