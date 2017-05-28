3ae19f28b761748ffef795993c30efc858d46466
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-05-27 20:38:01 -0400

    some small stuff

--- interceptor.c
+++ interceptor.c
@@ -520,8 +520,6 @@ static void exit_function(void)
 	sys_call_table[__NR_exit_group] = orig_exit_group;
 	set_addr_ro((unsigned long) sys_call_table);
 	spin_unlock(&calltable_lock);
-	spin_lock(&pidlist_lock);
-	spin_unlock(&pidlist_lock);
 }
 
 module_init(init_function);
