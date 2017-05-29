28871e0a0f44d808c9b7f96d4b1bb31594dd2e11
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-05-29 00:35:53 -0400

    additional blacklist

--- interceptor.c
+++ interceptor.c
@@ -409,7 +409,7 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 		} else {
 			printk( KERN_DEBUG "final monitor part\n" );
 			if (table[syscall].monitored == 2) {
-				if {check_pid_monitored(syscall,pid)} {
+				if (check_pid_monitored(syscall,pid)) {
 					spin_lock(&pidlist_lock);
 					del_pid_sysc(pid, syscall);
 					spin_unlock(&pidlist_lock);
